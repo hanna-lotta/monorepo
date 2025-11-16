8a0f6ff9ab3e366353ec23740697dcee21f017fd
Hanna Selden <hanna_lotta@hotmail.com>
2025-11-16 22:09:53 +0100

    auth -token validering

--- srcServer/data/auth.ts
+++ srcServer/data/auth.ts
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken'
+import { PayloadSchema } from './validation.js'
 
 const jwtSecret: string = process.env.JWT_SECRET || ''
 
-function createToken(userId: string, accesLevel: string): string {
+export function createToken(userId: string, accesLevel: string): string {
 	// Tiden sedan 1970-01-01 i sekunder
 	const now = Math.floor(Date.now() / 1000)
 
@@ -15,4 +16,31 @@ function createToken(userId: string, accesLevel: string): string {
 	}, jwtSecret)
 }
 
-export { createToken }
+	interface Payload  {
+		userId: string;
+		accesLevel: string;
+	}
+	
+	export function validateJwt(authHeader: string | undefined): Payload | null {
+	  // 'Bearer: token'
+	  if( !authHeader ) {
+		return null
+	  }
+	  const token: string = authHeader.substring(8)  // alternativ: slice, split, // 'Bearer: 
+	  try {
+		// Anropar jwt.verify (synchronous i jwt-biblioteket) som verifierar signaturen med hemligheten från env och returnerar det dekodade payload-objektet.
+		const decodedPayload = jwt.verify(token, process.env.JWT_SECRET || '') 
+		console.log('Decoded JWT', decodedPayload)
+	  const validatePayload = PayloadSchema.safeParse(decodedPayload);
+	  if (!validatePayload.success) {
+		console.log('Decoded JWT payload did not match schema');
+		return null;
+	  } 
+	  return validatePayload.data;
+	
+	  } catch(error) {
+		console.log('JWT verify failed: ', (error as any)?.message)
+		return null	
+	  }
+	}
+
